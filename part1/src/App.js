15e9bd20a2f79a7688ca1ff8a64d2fd30f9cd8ff
Jere Rignell <jrignell@student.hive.fi>
2021-05-29 17:22:08 +0300

    Javascriptia

--- part1/src/App.js
+++ part1/src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { v4 as uuidv4 } from 'uuid';
 
 const Header = (props) => {
   return (
@@ -9,18 +10,14 @@ const Header = (props) => {
 const Part = (props) => {
   return (
       <p>
-        {props.part} {props.exercises}
+        {props.name} {props.exercises}
       </p>
   );
 };
 
 const Content = (props) => {
   return (
-  <React.Fragment>
-      <Part part={props.part1} exercises={props.exercises1} />
-      <Part part={props.part2} exercises={props.exercises2} />
-      <Part part={props.part3} exercises={props.exercises3} />
-    </React.Fragment>
+      <Part name={props.name} exercises={props.exercises} />
   );
 };
 
@@ -31,20 +28,34 @@ const Total = (props) => {
 };
 
 const App = () => {
-  const course = 'Half Stack application development';
-  const part1 = 'Fundamentals of React';
-  const part2 = 'Using props to pass data';
-  const part3 = 'State of a component';
-  const exercises1 = 10;
-  const exercises2 = 7;
-  const exercises3 = 14;
+  const { name: renamedName, parts } = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  }
 
   return (
-    <div>
-      <Header course={course} />
-      <Content part1={part1} part2={part2} part3={part3} exercises1={exercises1} exercises2={exercises2} exercises3={exercises3} />
-      <Total exercises1={exercises1} exercises2={exercises2} exercises3={exercises3} />
-    </div>
+    <React.Fragment>
+      <Header course={renamedName} />
+       {parts.map(part => <div key={uuidv4()}><Content name={part.name} exercises={part.exercises} /></div>)}
+      <Total
+        exercises1={parts[0].exercises}
+        exercises2={parts[1].exercises}
+        exercises3={parts[2].exercises}
+      />
+    </React.Fragment>
   );
 }
 
